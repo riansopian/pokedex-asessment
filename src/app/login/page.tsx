806d9828a92c9bad105736f60ba19f606e2f1cba
Rian Sopian~ <riansopianccd@gmail.com>
2024-03-25 16:04:18 +0700

    update auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,8 @@ import React from "react";
 import ProviderButton from "@/components/ProviderButton";
 
 export const metadata = {
-	title: "Login | PokeLore",
-	description: "Login to PokeLore.",
+	title: "Login | PokeDex",
+	description: "Login to PokeDex.",
 };
 
 export default async function Login(): Promise<React.JSX.Element> {
@@ -16,7 +16,7 @@ export default async function Login(): Promise<React.JSX.Element> {
 			<div className="flex h-full flex-col items-center justify-center">
 				<div className="flex flex-col items-center justify-center space-y-10">
 					<Image src="/logo.png" width={200} height={200} alt={"PokeDex Logo"} />
-					<h1 className="text-4xl font-bold text-white">Welcome to PokeLore</h1>
+					<h1 className="text-4xl font-bold text-white">Welcome to PokeDex</h1>
 					{providers &&
 						Object.values(providers).map((provider) => (
 							<ProviderButton key={provider.name} id={provider.id} name={provider.name} callback={"/"} />
